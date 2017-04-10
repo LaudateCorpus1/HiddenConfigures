70731f66afe4f2555650c53d01b85102d7d14c0e
Aaron Liu <adl6585@rit.edu>
2017-04-09 21:23:31 -0400

    Added a help page when you click the help button

---
+++ QuizApp/app/src/main/java/wumbo/quizapp/DisplayHelpActivity.java
@@ -0,0 +1,13 @@
+package wumbo.quizapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DisplayHelpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_help);
+    }
+}
