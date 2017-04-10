15c8ff0ae6d4fd8e3e66e913cc08e7df80a5a2f6
ChristianBorges\Christian Borges <chrisborges121@gmail.com>
2017-04-10 19:19:39 -0400

    Initial screen loosely designed.

---
+++ UnityProject/Hidden Configures/Assets/Scripts/mainMenuButtons.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class mainMenuButtons : MonoBehaviour {
+
+	// Use this for initialization
+    public void Help_Button_Click()
+    {
+        SceneManager.LoadScene("Help");
+    }
+
+    public void Disclaimer_No_Continue_Button_Click()
+    {
+        SceneManager.LoadScene("Disclaimer_No_Continue");
+    }
+
+    public void Disclaimer_Continue_Button_Click()
+    {
+        SceneManager.LoadScene("Disclaimer_Continue");
+    }
+
+    public void Instructions_Button_Click()
+    {
+        SceneManager.LoadScene("Instructions");
+    }
+
+    public void Start_Button_Click()
+    {
+        SceneManager.LoadScene("Start");
+    }
+
+    public void Past_Results_Button_Click()
+    {
+        SceneManager.LoadScene("PastResults");
+    }
+}
