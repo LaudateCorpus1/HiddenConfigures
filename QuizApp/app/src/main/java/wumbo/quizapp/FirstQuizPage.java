69204b7077a33bdb8613ad2a2dc5fe254b73a09a
Philip Bedward <philipbed813@gmail.com>
2017-04-10 09:58:33 -0400

    Added Activity and onClick method for the start quiz button.

---
+++ QuizApp/app/src/main/java/wumbo/quizapp/FirstQuizPage.java
@@ -0,0 +1,21 @@
+package wumbo.quizapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class FirstQuizPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first_quiz_page);
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.TEST_MESSAGE);
+
+        TextView textView = (TextView) findViewById(R.id.textView);
+        textView.setText(message);
+    }
+}
