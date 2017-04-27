1d9ac1059edffb4c54582150815f591a80a5bdcd
chrisborges121 <cxb2262@g.rit.edu>
2017-04-26 21:15:53 -0400

    Adding else to else ifs fixed error text not going away problem.

--- UnityProject/Hidden Configures/Assets/Scripts/SwipeInput.cs
+++ UnityProject/Hidden Configures/Assets/Scripts/SwipeInput.cs
@@ -54,7 +54,7 @@ public class SwipeInput : MonoBehaviour
                 //StartCoroutine(SwipeError());
             }
         }
-        if (counter == 9)
+        else if (counter == 9)
         {
             GameObject term = GameObject.Find("Term 9");
             SwipeWord swipeWord = term.GetComponent<SwipeWord>();
@@ -70,7 +70,7 @@ public class SwipeInput : MonoBehaviour
                 //StartCoroutine(SwipeError());
             }
         }
-        if (counter == 8)
+        else if (counter == 8)
         {
             GameObject term = GameObject.Find("Term 8");
             SwipeWord swipeWord = term.GetComponent<SwipeWord>();
@@ -86,7 +86,7 @@ public class SwipeInput : MonoBehaviour
                 //StartCoroutine(SwipeError());
             }
         }
-        if (counter == 7)
+        else if (counter == 7)
         {
             GameObject term = GameObject.Find("Term 7");
             SwipeWord swipeWord = term.GetComponent<SwipeWord>();
@@ -102,7 +102,7 @@ public class SwipeInput : MonoBehaviour
                 //StartCoroutine(SwipeError());
             }
         }
-        if (counter == 6)
+        else if (counter == 6)
         {
             GameObject term = GameObject.Find("Term 6");
             SwipeWord swipeWord = term.GetComponent<SwipeWord>();
@@ -118,7 +118,7 @@ public class SwipeInput : MonoBehaviour
                 //StartCoroutine(SwipeError());
             }
         }
-        if (counter == 5)
+        else if (counter == 5)
         {
             GameObject term = GameObject.Find("Term 5");
             SwipeWord swipeWord = term.GetComponent<SwipeWord>();
@@ -134,7 +134,7 @@ public class SwipeInput : MonoBehaviour
                 //StartCoroutine(SwipeError());
             }
         }
-        if (counter == 4)
+        else if (counter == 4)
         {
             GameObject term = GameObject.Find("Term 4");
             SwipeWord swipeWord = term.GetComponent<SwipeWord>();
@@ -150,7 +150,7 @@ public class SwipeInput : MonoBehaviour
                 //StartCoroutine(SwipeError());
             }
         }
-        if (counter == 3)
+        else if (counter == 3)
         {
             GameObject term = GameObject.Find("Term 3");
             SwipeWord swipeWord = term.GetComponent<SwipeWord>();
@@ -166,7 +166,7 @@ public class SwipeInput : MonoBehaviour
                 //StartCoroutine(SwipeError());
             }
         }
-        if (counter == 2)
+        else if (counter == 2)
         {
             GameObject term = GameObject.Find("Term 2");
             SwipeWord swipeWord = term.GetComponent<SwipeWord>();
@@ -182,7 +182,7 @@ public class SwipeInput : MonoBehaviour
                 //StartCoroutine(SwipeError());
             }
         }
-        if (counter == 1)
+        else if (counter == 1)
         {
             GameObject term = GameObject.Find("Term 1");
             SwipeWord swipeWord = term.GetComponent<SwipeWord>();
